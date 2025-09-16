558945f1324096a4a659b7bcf1465e72fd19da61
Sylvain-TDA <sylvain.teissier@le-campus-numerique.fr>
2025-09-16 14:50:56 +0200

    refactor: tests

--- src/main/java/fr/welcome/Welcome.java
+++ src/main/java/fr/welcome/Welcome.java
@@ -27,17 +27,17 @@ public class Welcome {
         LocalTime morningEnd = LocalTime.of(13, 0);
         LocalTime afternoonEnd = LocalTime.of(18, 0);
 
-        boolean dayOfWeek = (dayOfTheWeek ==2 || dayOfTheWeek == 3 || dayOfTheWeek == 4 || dayOfTheWeek == 5 || dayOfTheWeek == 6);
-        boolean dayOfWeekEnd = (dayOfTheWeek==7 || dayOfTheWeek==1);
+        boolean dayOfWeek = (dayOfTheWeek == 2 || dayOfTheWeek == 3 || dayOfTheWeek == 4 || dayOfTheWeek == 5 || dayOfTheWeek == 6);
+        boolean dayOfWeekEnd = (dayOfTheWeek == 7 || dayOfTheWeek == 1);
 
         if (time.isAfter(morningStart) && time.isBefore(morningEnd) && dayOfWeek) {
             return "Bonjour";
-        } else if (time.isAfter(morningEnd) && time.isBefore(afternoonEnd)&& dayOfWeek) {
+        } else if (time.isAfter(morningEnd) && time.isBefore(afternoonEnd) && dayOfWeek) {
             return "Bon après-midi";
-        } else if (time.isAfter(afternoonEnd) && dayOfTheWeek != 6 && dayOfWeek) {
-            return "Bonsoir";
-        } else if (dayOfWeekEnd || (dayOfTheWeek == 6 && time.isAfter(afternoonEnd))) {
+        } else if (dayOfWeekEnd || (dayOfTheWeek == 6 && time.isAfter(afternoonEnd)) || (dayOfTheWeek == 2 && time.isBefore(morningStart))) {
             return "Bon week-end";
+        } else if (time.isAfter(afternoonEnd) || time.isBefore(morningStart) && dayOfWeek && dayOfTheWeek != 6) {
+            return "Bonsoir";
         } else {
             return "Out of bound";
         }
--- src/test/java/test/WelcomeTest.java
+++ src/test/java/test/WelcomeTest.java
@@ -2,6 +2,7 @@ package test;
 
 import fr.welcome.Welcome;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalTime;
@@ -9,6 +10,7 @@ import java.time.LocalTime;
 public class WelcomeTest {
 
     @Test
+    @DisplayName("Monday morning at 10AM")
     public void MorningTest() {
         FakeWelcome fakeWelcome = new FakeWelcome(LocalTime.of(10,0), 2);
         Welcome target = new Welcome(fakeWelcome);
@@ -17,6 +19,7 @@ public class WelcomeTest {
     }
 
     @Test
+    @DisplayName("Monday afternoon at 2PM")
     public void AfternoonTest() {
         FakeWelcome fakeWelcome = new FakeWelcome(LocalTime.of(14,0), 2);
         Welcome target = new Welcome(fakeWelcome);
@@ -25,6 +28,7 @@ public class WelcomeTest {
     }
 
     @Test
+    @DisplayName("Monday evening at 7PM")
     public void EveningTest() {
         FakeWelcome fakeWelcome = new FakeWelcome(LocalTime.of(19,0), 2);
         Welcome target = new Welcome(fakeWelcome);
@@ -33,10 +37,65 @@ public class WelcomeTest {
     }
 
     @Test
+    @DisplayName("Saturday morning at 10AM")
     public void WeekEndTest() {
         FakeWelcome fakeWelcome = new FakeWelcome(LocalTime.of(10,0), 7);
         Welcome target = new Welcome(fakeWelcome);
         String message = target.getMessage();
         Assertions.assertEquals("Bon week-end", message);
+    }
+
+    @Test
+    @DisplayName("Sunday morning at 10AM")
+    public void sundayTest() {
+        FakeWelcome fakeWelcome = new FakeWelcome(LocalTime.of(10,0), 1);
+        Welcome target = new Welcome(fakeWelcome);
+        String message = target.getMessage();
+        Assertions.assertEquals("Bon week-end", message);
+    }
+
+    @Test
+    @DisplayName("Monday morning before 9AM")
+    public void mondayMorningTest() {
+        FakeWelcome fakeWelcome = new FakeWelcome(LocalTime.of(8,59), 2);
+        Welcome target = new Welcome(fakeWelcome);
+        String message = target.getMessage();
+        Assertions.assertEquals("Bon week-end", message);
+    }
+
+    @Test
+    @DisplayName("Tuesday morning at 1AM")
+    public void tuesdayMorningTest() {
+        FakeWelcome fakeWelcome = new FakeWelcome(LocalTime.of(1,48), 3);
+        Welcome target = new Welcome(fakeWelcome);
+        String message = target.getMessage();
+        Assertions.assertEquals("Bonsoir", message);
+    }
+
+    @Test
+    @DisplayName("Wednesday morning before 9AM")
+    public void wednesdayBeforeMorningTest() {
+        FakeWelcome fakeWelcome = new FakeWelcome(LocalTime.of(8,59), 4);
+        Welcome target = new Welcome(fakeWelcome);
+        String message = target.getMessage();
+        Assertions.assertEquals("Bonsoir", message);
+    }
+
+    @Test
+    @DisplayName("Friday evening at 8PM")
+    public void fridayWeekEndTest() {
+        FakeWelcome fakeWelcome = new FakeWelcome(LocalTime.of(19,59), 6);
+        Welcome target = new Welcome(fakeWelcome);
+        String message = target.getMessage();
+        Assertions.assertEquals("Bon week-end", message);
+    }
+
+    @Test
+    @DisplayName("Friday afternoon at 4PM")
+    public void fridayAfternoonTest() {
+        FakeWelcome fakeWelcome = new FakeWelcome(LocalTime.of(15,59), 6);
+        Welcome target = new Welcome(fakeWelcome);
+        String message = target.getMessage();
+        Assertions.assertEquals("Bon après-midi", message);
     }
 }
